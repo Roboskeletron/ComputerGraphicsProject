738ddb7b7dab93ab8a1cb8e43e9036c1ad775913
SoniaFilozop <72881848+SoniaFilozop@users.noreply.github.com>
2023-11-11 19:38:59 +0300

    vectors fixed

--- math/src/main/java/com/cgp/math/vector/Vector2F.java
+++ math/src/main/java/com/cgp/math/vector/Vector2F.java
@@ -1,5 +1,60 @@
 package com.cgp.math.vector;
 
 public class Vector2F {
-    public float x, y;
+    private float x;
+    private float y;
+    private float magnitude = -1;
+
+    public Vector2F(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+        magnitude = -1;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+        magnitude = -1;
+    }
+
+    public Vector2F add(Vector2F other) {
+        return new Vector2F(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2F subtract(Vector2F other) {
+        return new Vector2F(this.x - other.x, this.y - other.y);
+    }
+
+    public Vector2F scalarMultiply(float scalar) {
+        return new Vector2F(this.x * scalar, this.y * scalar);
+    }
+
+    public float dotProduct(Vector2F other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    public float magnitude() {
+        if (magnitude >= 0) {
+            return magnitude;
+        }
+
+        magnitude = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+        return magnitude;
+    }
+
+    public Vector2F normalize() {
+        float magnitude = this.magnitude();
+        return new Vector2F(this.x / magnitude, this.y / magnitude);
+    }
 }
--- math/src/main/java/com/cgp/math/vector/Vector3F.java
+++ math/src/main/java/com/cgp/math/vector/Vector3F.java
@@ -1,5 +1,79 @@
 package com.cgp.math.vector;
 
 public class Vector3F {
-    public float x, y, z;
+    private float x;
+    private float y;
+    private float z;
+    private float magnitude = -1;
+
+    public Vector3F(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+        magnitude = -1;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+        magnitude = -1;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+        magnitude = -1;
+    }
+
+    public Vector3F add(Vector3F other) {
+        return new Vector3F(this.x + other.x, this.y + other.y, this.z + other.z);
+    }
+
+    public Vector3F subtract(Vector3F other) {
+        return new Vector3F(this.x - other.x, this.y - other.y, this.z - other.z);
+    }
+
+    public Vector3F scalarMultiply(float scalar) {
+        return new Vector3F(this.x * scalar, this.y * scalar, this.z * scalar);
+    }
+
+    public float dotProduct(Vector3F other) {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+
+    public float magnitude() {
+        if (magnitude >= 0) {
+            return magnitude;
+        }
+
+        magnitude = (float) Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+
+        return magnitude;
+    }
+
+    public Vector3F normalize() {
+        float magnitude = this.magnitude();
+        return new Vector3F(this.x / magnitude, this.y / magnitude, this.z / magnitude);
+    }
+
+    public Vector3F crossProduct(Vector3F other) {
+        float newX = this.y * other.z - this.z * other.y;
+        float newY = this.z * other.x - this.x * other.z;
+        float newZ = this.x * other.y - this.y * other.x;
+        return new Vector3F(newX, newY, newZ);
+    }
 }
