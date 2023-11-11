2469db78c3d0f8a03b049a9bfcee0a1a7ce93295
Roboskeletron <wwwrt287@gmail.com>
2023-11-11 14:29:13 +0300

    abstract vectors added

---
+++ math/src/main/java/com/cgp/math/vector/AbstractVector2F.java
@@ -0,0 +1,5 @@
+package com.cgp.math.vector;
+
+public abstract class AbstractVector2F {
+    public float x, y;
+}
---
+++ math/src/main/java/com/cgp/math/vector/AbstractVector3F.java
@@ -0,0 +1,5 @@
+package com.cgp.math.vector;
+
+public abstract class AbstractVector3F {
+    public float x, y, z;
+}
