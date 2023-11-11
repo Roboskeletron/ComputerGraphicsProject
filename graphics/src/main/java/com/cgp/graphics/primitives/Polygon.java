68e59ada957dbe841015698cf530c96e89947f17
Roboskeletron <wwwrt287@gmail.com>
2023-11-11 23:41:40 +0300

    methods added, builder added

--- graphics/src/main/java/com/cgp/graphics/primitives/Polygon.java
+++ graphics/src/main/java/com/cgp/graphics/primitives/Polygon.java
@@ -2,8 +2,59 @@ package com.cgp.graphics.primitives;
 
 import com.cgp.math.vector.Vector3F;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Polygon {
-    private ArrayList<Vector3F> vertices;
+    private final Vector3F[] vertices;
+
+    protected Polygon(Vector3F[] vertices) {
+        this.vertices = vertices;
+    }
+
+    public Vector3F getVertex(int index){
+        return vertices[index];
+    }
+
+    public int getVertexCount(){
+        return vertices.length;
+    }
+
+    public static class Builder {
+        private final LinkedList<Vector3F> vertices = new LinkedList<>();
+
+        public Builder(){
+
+        }
+
+        public Builder(Collection<Vector3F> vertices){
+            this.vertices.addAll(vertices);
+        }
+
+        public Builder(Vector3F... vertices){
+            this.vertices.addAll(Arrays.stream(vertices).toList());
+        }
+
+        public Builder withVertex(Vector3F vertex){
+            vertices.add(vertex);
+            return this;
+        }
+
+        public Builder withVertices(Collection<Vector3F> vertices){
+            this.vertices.addAll(vertices);
+            return this;
+        }
+
+        public Builder withVertices(Vector3F... vertices){
+            this.vertices.addAll(Arrays.stream(vertices).toList());
+            return this;
+        }
+
+        public Polygon build(){
+            return new Polygon(vertices.toArray(new Vector3F[0]));
+        }
+
+        public int getVertexCount(){
+            return vertices.size();
+        }
+    }
 }
