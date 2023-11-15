534bdfdffea0374229a7a56fa51ec1b3f01fd1c9
Roboskeletron <wwwrt287@gmail.com>
2023-11-15 17:18:10 +0300

    Vertex normal calculation

--- graphics/src/main/java/com/cgp/graphics/util/Normal.java
+++ graphics/src/main/java/com/cgp/graphics/util/Normal.java
@@ -4,6 +4,7 @@ import com.cgp.graphics.primitives.Polygon;
 import com.cgp.math.vector.Vector3F;
 
 import java.util.List;
+import java.util.Map;
 
 public class Normal {
     public static Vector3F getPolygonNormal(Polygon polygon){
@@ -17,4 +18,18 @@ public class Normal {
         return vector1.crossProduct(vector2).normalize();
     }
 
+    public static Vector3F getVertexNormal(Vector3F vertex, Map<Vector3F, List<Polygon>> vertexPolygonMap, Map<Polygon, Vector3F> polygonNormalMap){
+        var polygons = vertexPolygonMap.get(vertex);
+
+        Vector3F normal = new Vector3F(0, 0, 0);
+
+        for (var polygon : polygons){
+            var polygonNormal = polygonNormalMap.get(polygon);
+            normal = normal.add(polygonNormal);
+        }
+
+        normal = normal.scalarMultiply(1 / (float) polygons.size());
+
+        return normal;
+    }
 }
