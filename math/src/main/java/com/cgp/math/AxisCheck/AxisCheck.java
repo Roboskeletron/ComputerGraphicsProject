ea8cfa8971e97caa30ed658ddfa9b2f5d6db9849
Софья <soniafilozop20012005@gmail.com>
2023-11-15 22:54:27 +0300

    change class AxisCheck

--- math/src/main/java/com/cgp/math/AxisCheck/AxisCheck.java
+++ math/src/main/java/com/cgp/math/AxisCheck/AxisCheck.java
@@ -6,13 +6,19 @@ import java.util.List;
 
 public class AxisCheck {
 
-    public static boolean isVectorOnAxis(List<Vector3F> vectors) {
-        for (Vector3F vector : vectors) {
-            if (vector.getX() == 0 || vector.getY() == 0 || vector.getZ() == 0) {
-                return true;
-            }
+    public static Vector3F getDirectionIfVectorsOnSameLine(List<Vector3F> vectors) {
+        Vector3F crossProduct = vectors.get(0).crossProduct(vectors.get(1));
+
+        for (int i = 2; i < vectors.size(); i++) {
+            crossProduct = crossProduct.crossProduct(vectors.get(i));
         }
-        return false;
+
+        if (crossProduct.magnitude() == 0) {
+            return vectors.get(1).subtract(vectors.get(0));
+        }
+
+        return null;
     }
+
 }
 
