9a7d0baa404e7442dc5f4d913e903096d675c015
Roboskeletron <wwwrt287@gmail.com>
2023-11-16 18:47:43 +0300

    modify getPolygonNormal method

--- graphics/src/main/java/com/cgp/graphics/util/Normal.java
+++ graphics/src/main/java/com/cgp/graphics/util/Normal.java
@@ -1,6 +1,7 @@
 package com.cgp.graphics.util;
 
 import com.cgp.graphics.primitives.Polygon;
+import com.cgp.math.util.MathUtil;
 import com.cgp.math.vector.Vector3F;
 
 import java.util.List;
@@ -8,12 +9,29 @@ import java.util.Map;
 
 public class Normal {
     public static Vector3F getPolygonNormal(Polygon polygon){
+        if (polygon.getVertexCount() != 3){
+            throw new IllegalArgumentException("Cant calculate normal, polygon should be triangulated");
+        }
+
         var v0 = polygon.getVertex(0);
         var v1 = polygon.getVertex(1);
-        var v2 = polygon.getVertex(polygon.getVertexCount() - 1);
+        var v2 = polygon.getVertex(2);
 
         var vector1 = v1.subtract(v0);
         var vector2 = v2.subtract(v0);
+
+        var normal = vector1.crossProduct(vector2).normalize();
+
+        if (MathUtil.compareFloat(normal.magnitude(), 0) == 0){
+            return normal;
+        }
+
+        v0 = polygon.getVertex(1);
+        v1 = polygon.getVertex(2);
+        v2 = polygon.getVertex(0);
+
+        vector1 = v1.subtract(v0);
+        vector2 = v2.subtract(v0);
 
         return vector1.crossProduct(vector2).normalize();
     }
