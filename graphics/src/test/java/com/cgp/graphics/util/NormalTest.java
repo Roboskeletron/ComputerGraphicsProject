97698cdd679572282d9b68417c850a6c94e14713
Roboskeletron <wwwrt287@gmail.com>
2023-11-17 11:57:27 +0300

    getPolygonNormal from vertices normals test

--- graphics/src/test/java/com/cgp/graphics/util/NormalTest.java
+++ graphics/src/test/java/com/cgp/graphics/util/NormalTest.java
@@ -46,6 +46,13 @@ class NormalTest {
         assertEquals(expectedNormal, calculatedNormal);
     }
 
+    @ParameterizedTest
+    @MethodSource
+    void getPolygonNormalFromVertices(Polygon polygon, Map<Vector3F, Vector3F> vertexNormalMap, Vector3F expectedNormal) {
+        Vector3F calculatedNormal = Normal.getPolygonNormal(polygon, vertexNormalMap);
+        assertEquals(expectedNormal, calculatedNormal);
+    }
+
     private static Stream<Arguments> getPolygonNormal() {
         return Stream.of(
                 Arguments.of(
@@ -124,6 +131,38 @@ class NormalTest {
                 Arguments.of(vertex1, vertexPolygonMap, polygonNormalMap, new Vector3F(1, 1, 1)),
                 Arguments.of(vertex2, vertexPolygonMap, polygonNormalMap, new Vector3F(0.75f, 0.5f, 0.35f)),
                 Arguments.of(vertex3, vertexPolygonMap, polygonNormalMap, new Vector3F(0.166666666f, 0.4f, 0.266666666f))
+        );
+    }
+
+    private static Stream<Arguments> getPolygonNormalFromVertices() {
+        Vector3F vertex1 = new Vector3F(0, 1, 0);
+        Vector3F vertex2 = new Vector3F(1, 1, 1);
+        Vector3F vertex3 = new Vector3F(2, 2, 2);
+        Vector3F vertex4 = new Vector3F(0, 0, 0);
+        Vector3F vertex5 = new Vector3F(1, 1, 1);
+        Vector3F vertex6 = new Vector3F(2, 2, 2);
+
+        Polygon polygon1 = new Polygon.Builder(vertex1, vertex2, vertex3)
+                .build();
+
+        Polygon polygon2 = new Polygon.Builder(vertex2, vertex3, vertex5)
+                .build();
+
+        Polygon polygon3 = new Polygon.Builder(vertex1, vertex2, vertex3, vertex4, vertex5, vertex6)
+                .build();
+
+        Map<Vector3F, Vector3F> vertexNormalMap = new HashMap<>();
+        vertexNormalMap.put(vertex1, new Vector3F(0, 0, 1));
+        vertexNormalMap.put(vertex2, new Vector3F(1, 0, 0));
+        vertexNormalMap.put(vertex3, new Vector3F(0, 1, 0));
+        vertexNormalMap.put(vertex4, new Vector3F(0.5f, -1, 1));
+        vertexNormalMap.put(vertex5, new Vector3F(1, -0.45f, 0.3f));
+        vertexNormalMap.put(vertex6, new Vector3F(-1, -1, 0.3f));
+
+        return Stream.of(
+                Arguments.of(polygon1, vertexNormalMap, new Vector3F(1f / 3, 1f / 3, 1f / 3)),
+                Arguments.of(polygon2, vertexNormalMap, new Vector3F(2f / 3, 0.55f / 3, 0.3f / 3)),
+                Arguments.of(polygon3, vertexNormalMap, new Vector3F(1.5f / 6, -1.45f / 6, 2.6f / 6))
         );
     }
 }
