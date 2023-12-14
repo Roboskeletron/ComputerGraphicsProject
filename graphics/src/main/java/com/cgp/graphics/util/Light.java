b7946ab0f9ef411b0e6509b1833204df2ee6e69a
Roboskeletron <wwwrt287@gmail.com>
2023-12-14 17:01:09 +0300

    apply direct light

---
+++ graphics/src/main/java/com/cgp/graphics/util/Light.java
@@ -0,0 +1,31 @@
+package com.cgp.graphics.util;
+
+import com.cgp.graphics.primitives.ColoredPoint;
+import com.cgp.math.vector.Vector3F;
+import javafx.scene.paint.Color;
+
+public class Light {
+    public static void applyDirectLight(Vector3F ray, ColoredPoint vertex, Vector3F normal, float k) {
+        if (k < 0 || k > 1) {
+            throw new IllegalArgumentException("k must be between 0 and 1 (inclusive)");
+        }
+
+        var l = normal.scalarMultiply(-1).dotProduct(vertex);
+
+        var color = vertex.getColor();
+        var rgb = new Vector3F((float) color.getRed(), (float) color.getGreen(), (float) color.getBlue());
+
+        rgb = rgb.scalarMultiply(1 - k)
+                .add(
+                        rgb.scalarMultiply(k * l)
+                )
+        ;
+
+        vertex.setColor(new Color(
+                rgb.getX(),
+                rgb.getY(),
+                rgb.getZ(),
+                1)
+        );
+    }
+}
