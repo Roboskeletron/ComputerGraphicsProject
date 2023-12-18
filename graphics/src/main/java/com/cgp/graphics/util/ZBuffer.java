649b4b2a8940a2c6df8dbc643049f9d2cf1587fd
Roboskeletron <wwwrt287@gmail.com>
2023-12-18 22:11:46 +0300

    save mapping

--- graphics/src/main/java/com/cgp/graphics/util/ZBuffer.java
+++ graphics/src/main/java/com/cgp/graphics/util/ZBuffer.java
@@ -8,25 +8,26 @@ import com.cgp.math.vector.Vector3F;
 import java.util.*;
 
 public class ZBuffer {
-    public static Collection<Vector3F> filterPoints(Collection<Vector3F> points){
-        HashMap<Vector2F, Vector3F> buffer = new HashMap<>();
+    public static Collection<Map.Entry<Vector3F, Vector3F>> filterPoints(Map<Vector3F, Vector3F> points){
+        HashMap<Vector2F, Map.Entry<Vector3F, Vector3F>> buffer = new HashMap<>();
 
-        points.stream().forEachOrdered(point -> ZBuffer.setValue(buffer, point));
+        points.entrySet().stream().forEachOrdered(point -> ZBuffer.setValue(buffer, point));
 
         return buffer.values();
     }
 
-    private static void setValue(Map<Vector2F, Vector3F> buffer, Vector3F point){
-        Vector2F key = new Vector2F(point.getX(), point.getY());
+    private static void setValue(Map<Vector2F, Map.Entry<Vector3F, Vector3F>> buffer,
+                                 Map.Entry<Vector3F, Vector3F> point){
+        Vector2F key = new Vector2F(point.getValue().getX(), point.getValue().getY());
 
         if (!buffer.containsKey(key)){
             buffer.put(key,  point);
             return;
         }
 
-        float depth = buffer.get(key).getZ();
+        float depth = buffer.get(key).getValue().getZ();
 
-        if (MathUtil.compareFloat(point.getZ(), depth) < 0){
+        if (MathUtil.compareFloat(point.getValue().getZ(), depth) < 0){
             buffer.put(key, point);
         }
     }
