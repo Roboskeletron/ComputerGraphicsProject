fc6ea1f329c681208540985f9883f821e67c5f7d
Roboskeletron <wwwrt287@gmail.com>
2023-12-21 19:10:43 +0300

    fully parallel calculations

--- graphics/src/main/java/com/cgp/graphics/util/ZBuffer.java
+++ graphics/src/main/java/com/cgp/graphics/util/ZBuffer.java
@@ -6,27 +6,28 @@ import com.cgp.math.vector.Vector2F;
 import com.cgp.math.vector.Vector3F;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ZBuffer {
-    public static <Vector extends Vector3F> Collection<Vector> filterPoints(Collection<Vector> points){
-        HashMap<Vector2F, Vector> buffer = new HashMap<>();
+    public static <Vector extends Vector3F> Collection<Vector> filterPoints(Collection<Vector> points) {
+        ConcurrentHashMap<Vector2F, Vector> buffer = new ConcurrentHashMap<>();
 
-        points.stream().forEachOrdered(point -> ZBuffer.setValue(buffer, point));
+        points.parallelStream().forEach(point -> ZBuffer.setValue(buffer, point));
 
         return buffer.values();
     }
 
-    private static <Vector extends Vector3F> void setValue(Map<Vector2F, Vector> buffer, Vector point){
+    private static <Vector extends Vector3F> void setValue(Map<Vector2F, Vector> buffer, Vector point) {
         Vector2F key = new Vector2F(point.getX(), point.getY());
 
-        if (!buffer.containsKey(key)){
-            buffer.put(key,  point);
+        if (!buffer.containsKey(key)) {
+            buffer.put(key, point);
             return;
         }
 
         float depth = buffer.get(key).getZ();
 
-        if (MathUtil.compareFloat(point.getZ(), depth) < 0){
+        if (MathUtil.compareFloat(point.getZ(), depth) < 0) {
             buffer.put(key, point);
         }
     }
