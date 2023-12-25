836e79ac0e7228f0aee492a3c945044b088308ff
Roboskeletron <wwwrt287@gmail.com>
2023-12-25 17:52:04 +0300

    view matrix primitive added

---
+++ graphics/src/main/java/com/cgp/graphics/primitives/rasterization/ViewMatrix.java
@@ -0,0 +1,22 @@
+package com.cgp.graphics.primitives.rasterization;
+
+import com.cgp.graphics.entities.Camera;
+import com.cgp.graphics.entities.GameObject;
+import com.cgp.graphics.util.Rasterization;
+import com.cgp.math.matrix.Matrix4;
+
+public final class ViewMatrix {
+    private final GameObject gameObject;
+    private final Matrix4 viewMatrix;
+
+    private ViewMatrix(GameObject gameObject, Matrix4 viewMatrix) {
+        this.gameObject = gameObject;
+        this.viewMatrix = viewMatrix;
+    }
+
+    public static ViewMatrix create(GameObject target, Camera camera){
+        var viewMatrix = Rasterization.lookAt(target, camera);
+
+        return new ViewMatrix(target, viewMatrix);
+    }
+}
