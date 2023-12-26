9cc31054ef75419fde07fa7ef10ca0017c0a8a4d
Roboskeletron <wwwrt287@gmail.com>
2023-12-26 22:27:55 +0300

    implement TexturePipeline

--- graphics/src/main/java/com/cgp/graphics/pipeline/TexturePipeline.java
+++ graphics/src/main/java/com/cgp/graphics/pipeline/TexturePipeline.java
@@ -1,14 +1,24 @@
 package com.cgp.graphics.pipeline;
 
-import com.cgp.graphics.entities.GameObject;
 import com.cgp.graphics.entities.Scene;
-import com.cgp.graphics.entities.Camera;
-import com.cgp.graphics.primitives.rasterization.BarycentricVector;
-import com.cgp.math.vector.Vector3F;
+import com.cgp.graphics.primitives.rasterization.ColoredPoint;
 import javafx.scene.canvas.GraphicsContext;
 
 public class TexturePipeline extends BasicPipeline {
     public TexturePipeline(Scene scene) {
         super(scene);
+    }
+
+    @Override
+    protected void cleanScreen(GraphicsContext graphicsContext) {
+        var canvas = graphicsContext.getCanvas();
+        graphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    }
+
+    @Override
+    protected void drawPoint(ColoredPoint point, GraphicsContext graphicsContext) {
+        int x = (int) point.getX();
+        int y = (int) point.getY();
+        graphicsContext.getPixelWriter().setColor(x, y, point.getColor());
     }
 }
